767a0cafbeb4e2b745a44ee45a3de449b06e1092
Daniel Fischer <daniel.altavilla94@gmail.com>
2025-03-15 15:18:10 +0100

    create nav-bar

---
+++ heroes/src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+import { NavBar } from './components/NavBar';
+
+function App() {
+
+
+  return (
+    <>
+      <div className="A
---
+++ heroes/src/components/NavBar.jsx
@@ -0,0 +1,6 @@
+import menuIcon from "../assets/menu-icon.png"
+import discordLogo from "../assets/discord-logo-white.png"
+
+export function NavBar () {
+    return (
+        <nav cl
